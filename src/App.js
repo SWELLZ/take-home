312fe07cf2565ffb69ff98ea4b5db162cf1bfc3a
SWELLZ <microsoftdavid4@gmail.com>
2022-11-02 10:46:50 -0700

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 
 function App() {
-  return null;
+  return (
+    <>
+    <div className="flex justify-between h-[70px] w-full bg-red-800 text-white items-center">
+      <a className="ml-8 font-bold text-2xl" href="">COMPANY</a>
+      <nav className="w-[30%] flex justify-evenly mr-8">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Support</a>
+      </nav>
+    </div>
+    <section>
+      <h1>This company does... Stuff?</h1>
+    </section>
+    </>
+  );
 }
 
 export default App;
