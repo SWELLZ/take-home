20bf2ba5c3ffaa97c8723aa1e6db26637d55c7f0
SWELLZ <microsoftdavid4@gmail.com>
2022-11-02 11:00:09 -0700

    add landing section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 function App() {
   return (
     <>
-    <div className="flex justify-between h-[70px] w-full bg-red-800 text-white items-center">
+    <div className="flex justify-between h-[70px] w-full bg-red-800 text-white items-center absolute top-0">
       <a className="ml-8 font-bold text-2xl" href="">COMPANY</a>
       <nav className="w-[30%] flex justify-evenly mr-8">
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Support</a>
+        <a className="underline hover:no-underline" href="">Home</a>
+        <a className="underline hover:no-underline" href="">About</a>
+        <a className="underline hover:no-underline" href="">Support</a>
       </nav>
     </div>
-    <section>
-      <h1>This company does... Stuff?</h1>
+    <section className="bg-[url('components/resources/landing-sect.jpg')] h-screen pt-[70px] text-white bg-no-repeat bg-center bg-cover">
+      <div className="w-full h-full grid place-items-center bg-black/30">
+        <h1 className="text-5xl bg-gradient-to-tr from-purple-200 via-pink-200 to-blue-200 bg-clip-text text-transparent font-bold">This company does... Stuff?</h1>
+      </div>
     </section>
     </>
   );
