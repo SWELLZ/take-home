de0748be1f4415650668988227483be967afc60a
SWELLZ <microsoftdavid4@gmail.com>
2022-11-02 12:00:56 -0700

    add expanded styling

--- src/components/mainComponent/Main.jsx
+++ src/components/mainComponent/Main.jsx
@@ -3,6 +3,7 @@ import { About } from '../aboutComponent/About';
 import { Landing } from '../landingComponents/Landing';
 import { Nav } from '../navComponent/Nav';
 import message from '../resources/message-icon.png';
+import close from '../resources/close.png'
 
 
 export function Main() {
@@ -12,27 +13,40 @@ export function Main() {
         setExpanded(!expanded)
     }
 
-    const expandedCSS = expanded ? 'bottom-4 right-4 fixed bg-white w-24 h-24 rounded-full border-2' : 'w-[400px] h-[500px] bottom-4 right-4 fixed bg-white border-2 rounded-md'
+    const expandedCSS = expanded ? 'bottom-4 right-4 fixed bg-white w-24 h-24 rounded-full border-2' : 'w-[400px] h-[500px] bottom-4 right-4 fixed bg-white border-2 rounded-md bg-gray-200'
 
     return (
         <div className='relative'>
             <Nav />
             <Landing />
             <About />
-            <div>
-                <div className={expandedCSS}>
-                    {/* TOGGLE EXPAND BUTTON --- START */}
-                    {expanded ? 
-                        <button onClick={handleToggle} className='w-full h-full  grid place-items-center'>
-                            <img src={message} alt='chat indicator' className='w-3/5 pointer-events-none' />
-                        </button>
-                        :
-                        <button onClick={handleToggle} className='w-4 h-4 bg-black'>
+            <div className={expandedCSS}>
+                {/* TOGGLE EXPAND BUTTON --- START */}
+                {expanded ? 
+                    <button onClick={handleToggle} className='w-full h-full  grid place-items-center'>
+                        <img src={message} alt='chat indicator' className='w-3/5 pointer-events-none' />
+                    </button>
+                    :
+                    <button onClick={handleToggle} className='mt-2 ml-2 w-4 h-4'>
+                        <img className="w-full" src={close} />
+                    </button>
+                }
+                {/* TOGGLE EXPAND BUTTON --- END */}
+                {!expanded ? 
+                <section className="px-2 py-2 h-full">
+                    <h2 className="text-center text-lg font-semibold">Welcome to the Chat Bot</h2>
 
-                        </button>
-                    }
-                    {/* TOGGLE EXPAND BUTTON --- END */}
-                </div>
+                    <div className="bg-white mt-4 h-4/5 w-full flex flex-col-reverse">
+                        {/* EACH MESSAGE */}
+                        <div>
+                            <p>asdf</p>
+                        </div>
+                    </div>
+
+                </section>
+                :
+                null
+                }
             </div>
         </div>
     );
